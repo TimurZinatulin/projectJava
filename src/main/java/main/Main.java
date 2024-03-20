a890bcc516215d709f9e51f650ce15ff3619018d
Admin <cvbvitmitm45@gmail.com>
2024-03-21 03:12:04 +0500

    HW02 completed

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,35 @@
+package main;
+
+import accounts.AccountService;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.MirrorRequestsServlet;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        AccountService accountService = new AccountService();
+
+
+
+        MirrorRequestsServlet mirrorRequestsServlet = new MirrorRequestsServlet();
+        SignInServlet signInServlet = new SignInServlet(accountService);
+        SignUpServlet signUpServlet = new SignUpServlet(accountService);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(mirrorRequestsServlet), "/");
+        context.addServlet(new ServletHolder(signInServlet), "/signin");
+        context.addServlet(new ServletHolder(signUpServlet), "/signup");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
+        server.join();
+        //System.out.print();
+    }
+}
